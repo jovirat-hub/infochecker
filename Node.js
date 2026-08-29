0fd8bb74015a0d2ddefbc3ffa50675c3f2fdf8f5
jovirat-hub <jovi.flores.ui@phinmaed.com>
2026-08-29 07:36:30 +0100

    Delete Node.js

--- Node.js
+++
@@ -1,8 +0,0 @@
-# Check if Node.js is installed
-docker exec -it waf-solver node --version
-
-# Check if npm is installed
-docker exec -it waf-solver npm --version
-
-# Check the path
-docker exec -it waf-solver which node
